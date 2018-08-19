2c7a651ac48917e80c22d8b9bc1e8562f9ba0bcd
betteridiot <mdsherman@betteridiot.tech>
2018-08-19 19:47:29 -0400

    blah

--- basic_script.py
+++ basic_script.py
@@ -1,5 +1,3 @@
-# %load basic_script.py
-
 """Write a Python 'Hello, World' program.
 
 A 'Hello, World' program is a program that prints out 'Hello, World' on the screen.
@@ -10,4 +8,3 @@ In addition to doing 'Hello, World', I want you to go a little further.
 1. Save your partner's name as a variable
 2. Print out 'Hello, <your partner's name>' by passing it the variable
 """
-print('Hello, World')

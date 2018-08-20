c554ecd29153b809c7c1ee2463b99446cdfc0e98
Marcus D Sherman <betteridiot@users.noreply.github.com>
2018-08-20 00:24:30 -0400

    Update init.py

--- utils/init.py
+++ utils/init.py
@@ -1,3 +1,8 @@
 import os
-os.environ['CULL_TIMEOUT'] = "28800"
-os.environ['CULL_KERNEL_TIMEOUT'] = "28800"
+import shutil
+import HTML
+
+print('Resetting kernel environmental variables ...') 
+shutil.move(os.path.join(os.getcwd(), 'utils', 'init.py'),
+            os.path.join(os.getcwd(), '.ipython', 'profile_default', 'startup', 'init.py'))
+print(html.unescape("Kernel environment variables reset. Please press '&#8635;' above or '0' on your keyboard"))

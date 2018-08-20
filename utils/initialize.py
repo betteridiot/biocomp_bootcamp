a93cc600d59e96b43dc66938bedfb2ccc3e777e4
Marcus D Sherman <betteridiot@users.noreply.github.com>
2018-08-20 00:48:52 -0400

    Update initialize.py

--- utils/initialize.py
+++ utils/initialize.py
@@ -6,11 +6,11 @@ import html
 try:
     shutil.move(os.path.join(os.getcwd(), 'utils', 'init.py'),
                 os.path.join(os.getcwd(), '.ipython', 'profile_default', 'startup', 'init.py'))
-    print(html.unescape("Kernel environment variables reset. Please press the '&#8635;' button above or '0' twice on your keyboard"))
+    print(html.unescape("Kernel environment variables reset.\nPlease press the '&#8635;' button above or '0' twice on your keyboard"))
 except FileNotFoundError:
     if os.path.isfile(os.path.join(os.getcwd(), '.ipython', 'profile_default', 'startup', 'init.py')):
         if os.getenv('CULL_TIMEOUT') == '28800':
-            print('Kernel environmental variables already set. Please continue.')
+            print('Kernel environmental variables already set.\nPlease continue.')
         else:
-            print(html.unescape("Kernel environment variables already reset. Please press '&#8635;' above or '0' on your keyboard"))
+            print(html.unescape("Kernel environment variables already reset.\nPlease press '&#8635;' above or '0' twice on your keyboard"))
 
